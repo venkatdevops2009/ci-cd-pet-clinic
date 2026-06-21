591579cd3aca3068e6c56b9f57d8f620c21433b5
Venkat <venkatdevops2009@gmail.com>
2026-06-21 22:21:59 +0530

    intial commit

--- Infra/ec2.tf
+++ Infra/ec2.tf
@@ -58,6 +58,13 @@ resource "aws_security_group" "db_sg" {
   description = "Allow DB access from Java server"
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
